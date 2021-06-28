0297e44aded644128d8242e71dbfba6a0e070eeb
Andrei Borovik <andrei.borovik2000@yandex.by>
2021-06-28 15:29:42 +0300

    Added service

---
+++ Models/Company.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+namespace Task1.Models
+{
+    public class Company : Entity
+    {
+        public string Name { get; set; }
+        public List<User> Users { get; set; } = new();
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Task1.Models
+{
+    public class User : Entity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
